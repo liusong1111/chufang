3abdc677ba3c8855e36dbdc32e1b48f89655a677
liusong1111 <liusong1111@gmail.com>
2015-04-18 20:38:13 +0800

    production build

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@
 CROSSWALK_PATH=/Users/sliu/Downloads/crosswalk-11.40.277.7
 
 #CROSSWALK_PATH=/Users/sliu/Downloads/crosswalk-13.41.313.0
-#python $CROSSWALK_PATH/make_apk.py --mode=shared --package=com.lianliankan --manifest=resources/public/manifest.json --arch=arm --enable-remote-debugging
-python $CROSSWALK_PATH/make_apk.py --mode=embedded --package=com.lianliankan --manifest=resources/public/manifest.json --arch=arm --enable-remote-debugging
+python $CROSSWALK_PATH/make_apk.py --mode=shared --package=com.lianliankan --manifest=resources/public/manifest.json --arch=arm #--enable-remote-debugging
 
-#--keystore-path=./ks2.store --keystore-alias=./ks2.store
 
